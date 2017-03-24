4ffc143237ef3e042973cd00bb6eeda5a07c9bd9
Drew Larson <drewelizabeth@Drews-MacBook-Air.local>
2017-03-24 08:24:11 -0600

    testing branch creation

--- basic-js-review/review.js
+++ basic-js-review/review.js
@@ -1,23 +1,27 @@
 /////////////Variables/////////////
 //1. Write a variable called color with a value of "dark blue".
-
+var color = 'dark blue';
 //2. Write a variable, myFave, that has the value "My favorite color is "
-
+var myFave = 'My favorite color is';
 //3. Use the variables from 1 and 2 to make myFavoriteColor equal
 //   "My favorite color is dark blue" (concatenate)
-var myFavoriteColor;
+var myFavoriteColor = myFave + ' ' + color;
 
 //4. Create an array of strings that includes the letters a - d in order.
-var aThroughD;
+var aThroughD = ['a', 'b', 'c', 'd'];
 //5. Remove 'a' and store it to a variable called isForApple.
-
+var isForApple = aThroughD.shift();
 //6. Add 'e' to the end of aThroughD.
-
+aThroughD.push('e');
 //7. Add 'apple' to the beginning of aThroughD
-
+aThroughD.unshift('apple');
 //8. Remove 'c' from the array and put 'cat' in its place
 
+aThroughD.splice(aThroughD.indexOf('c'), 1, 'cat');
+
 //9. Leave 'apple' in the array but put a copy of it on a var called bestFruit
+
+var bestFruit = aThroughD.slice(0, 1).join();
 
 //10. Create an object called spaceJam
 //    spaceJam has: a title of 'Space Jam' (String)
@@ -35,32 +39,58 @@ var anakinSkywalker =  {
   former_significant_other: 'Padme Amidala',
   former_mother: 'Shmi Skywalker'
 }
-
+anakinSkywalker['side'] = 'dark';
 //12. Add 'Jar Jar' to his children using dot notation
-
+anakinSkywalker.children.push('Jar Jar');
 //13. Loop through the object and remove any property that starts with former
 
-
+for (var prop in anakinSkywalker) {
+  if (prop.indexOf('former') >= 0) {
+    delete anakinSkywalker[prop];
+  }
+}
 
 //14. Create a function called finder that takes in an array and a string and loops through the array
 //    to remove every instance of that string
 
-
+function finder(arr, str) {
+  if (arr.indexOf(str) >= -1) {
+    arr.splice(arr.indexOf(str), 1);
+  }     
+   return arr;
+}
 
 //15. Write a function called numChecker that takes in a parameter and checks to see if it is a number or a
   //number in a string (ie '12' or 12) - if so, return true, else return false
 
-
+function numChecker(val) {
+  if (Number(val)) {
+    return true;
+  } 
+    return false;
+  
+}
 
 //16. Write a function called rightNow that returns a new date object
 
-
+function rightNow() {
+  return new Date();
+}
 
 //17. Write a function called whoAmI that takes in an object and returns the value of the
 //    object's name property, and if the object's name
 //    is undefined, return 'has amnesia'.
 
-
+function whoAmI(obj) {
+  
+  if (obj.name === undefined) {
+    return 'has amnesia';
+  } return obj.name;
+}
 
 //18. Write a function called returnMe that takes in a num and a callback function and
 //    returns the result of the callback function invoked with the num as a parameter
+
+function returnMe(num, cb) {
+  return cb(num);
+}
